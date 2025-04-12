0dc19b435c30388f9ad927b09408a99011b37331
dhruv <dhruvsaini230@gmail.com>
2025-04-13 02:16:33 +0530

    update/modified codebase

--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -8,13 +8,13 @@ import {getAuth} from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBJkUxklKJgj4DpkO0oHm-UxR-vxwryVyM",
-  authDomain: "healthplum-d7c5a.firebaseapp.com",
-  projectId: "healthplum-d7c5a",
-  storageBucket: "healthplum-d7c5a.firebasestorage.app",
-  messagingSenderId: "205884242786",
-  appId: "1:205884242786:web:32104ff1921a9100d68a3b",
-  measurementId: "G-ZWH4H37K06"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID ,
+  measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
 const app = initializeApp(firebaseConfig);
